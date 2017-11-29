29c8896130ef5c8cec08318c2329cbaee310cd3a
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2017-11-29 11:56:15 +0530

    for loop changed

--- Day2-GitDemo/src/main/java/org/cap/demo/OrderClass.java
+++ Day2-GitDemo/src/main/java/org/cap/demo/OrderClass.java
@@ -4,9 +4,12 @@ public class OrderClass {
 	int[] arr={1,23,45,-90,12};
 	
 	public void display(){
-		
-		for(int i=0;i<arr.length;i++)
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
+			sum+=arr[i];
 			System.out.println(arr[i]);
+		}
+		System.out.println("Sum = " +sum);
 	}
 	
 	public void show(){
