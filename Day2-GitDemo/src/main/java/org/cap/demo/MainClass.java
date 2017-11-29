49f0300ffe6dffdc1563b06073f20ff6f477f068
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2017-11-29 10:13:45 +0530

    Commit All

---
+++ Day2-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		OrderClass obj=new OrderClass();
+		obj.display();
+		greetings();
+	}
+
+	
+	public static void greetings(){
+		System.out.println("Good Morning");
+	}
+}
---
+++ Day2-GitDemo/src/main/java/org/cap/demo/OrderClass.java
@@ -0,0 +1,12 @@
+package org.cap.demo;
+
+public class OrderClass {
+	int[] arr={1,23,45,-90,12};
+	
+	public void display(){
+		
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+
+}
